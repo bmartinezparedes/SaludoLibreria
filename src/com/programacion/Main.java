81eef9c71647b85bf32a609929c43a8f7f22afac
bmartinezparedes <bmartinezparedes@danielcastelao.org>
2021-02-09 11:47:45 +0100

    libreria añadir

--- src/com/programacion/Main.java
+++ src/com/programacion/Main.java
@@ -1,8 +1,10 @@
 package com.programacion;
-
+import com.Braiskiskos.*;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        MisMetodos.saludo();
     }
 }
